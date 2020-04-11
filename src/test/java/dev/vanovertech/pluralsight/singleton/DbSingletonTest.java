981e98d7de72fc8b21c1d605c336aebfecd9f08f
tylervanover <yeti.jones@gmail.com>
2020-04-10 22:24:19 -0500

    Initial commit w/ Singleton Pattern

---
+++ src/test/java/dev/vanovertech/pluralsight/singleton/DbSingletonTest.java
@@ -0,0 +1,38 @@
+package dev.vanovertech.pluralsight.singleton;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * A class that tests the {@link DbSingleton class}
+ */
+public class DbSingletonTest {
+
+    @Test
+    @DisplayName("DbSingleton returns same reference on two successive getInstance()")
+    public void test_getInstance_returnsSameReference() {
+        DbSingleton one = DbSingleton.getInstance();
+        DbSingleton two = DbSingleton.getInstance();
+
+        assertThat(one, is(two));
+    }
+
+    @Test
+    @DisplayName("DbSingleton constructor cannot be accessed via reflection")
+    public void test_singletonConstructor_isInaccessible() {
+        DbSingleton one = DbSingleton.getInstance();
+        assertThrows(InvocationTargetException.class, () -> {
+            for (Constructor constructor : DbSingleton.class.getDeclaredConstructors()) {
+                constructor.setAccessible(true);
+                DbSingleton instance = (DbSingleton) constructor.newInstance();
+            }
+        });
+    }
+}

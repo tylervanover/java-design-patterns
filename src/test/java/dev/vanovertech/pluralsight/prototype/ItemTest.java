bbdc0ef9be2219e455f0a5110ca7b6c8f7aedab5
tylervanover <yeti.jones@gmail.com>
2020-04-11 18:10:26 -0500

    Add Prototype pattern
    * Implemented a naive Prototype approach that left us with shallow
    copies
    * Re-implemented using a Prototype pattern
    * Added tests for the Item abstract class using Mockito
    * Added tests for concrete implementations of Book and Movie
    * Added tests for the Registry object which setup prototypes and called
    the clone method under the hood

---
+++ src/test/java/dev/vanovertech/pluralsight/prototype/ItemTest.java
@@ -0,0 +1,31 @@
+package dev.vanovertech.pluralsight.prototype;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Tests for the abstract class {@link Item}
+ */
+public class ItemTest {
+
+    private final double PRICE = 1.99;
+    private final String TITLE = "Title";
+    private final String URL = "https://www.example.com";
+
+    @Test
+    @DisplayName("Abstract class' methods called from Mock")
+    public void givenConcreteMethod_whenMocked_testCorrectBehavior() {
+        Item item = Mockito.mock(Item.class, Mockito.CALLS_REAL_METHODS);
+        item.setPrice(PRICE);
+        item.setTitle(TITLE);
+        item.setUrl(URL);
+
+        assertThat(item.getPrice(), is(PRICE));
+        assertThat(item.getTitle(), is(TITLE));
+        assertThat(item.getUrl(), is(URL));
+    }
+}
